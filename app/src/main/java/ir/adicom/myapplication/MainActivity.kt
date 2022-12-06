f5d35d2266d3e8ab8cbed52c3980a62ba761e20c
adicomdotir <adicom.ir@gmail.com>
2022-12-06 22:41:51 +0330

    step-2

--- app/src/main/java/ir/adicom/myapplication/MainActivity.kt
+++ app/src/main/java/ir/adicom/myapplication/MainActivity.kt
@@ -5,19 +5,21 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ir.adicom.myapplication.ui.theme.Compose_uiTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,31 +35,218 @@ class MainActivity : ComponentActivity() {
 //                ) {
 //                    Greeting("Android")
 //                }
-                Greeting(name = "Android")
+                Greeting()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    val context = LocalContext.current
-    var clicked by remember {
-        mutableStateOf(false)
-    }
+fun Greeting() {
+    LazyColumn(Modifier.fillMaxSize()) {
+        item {
+            TutorialHeader(text = "Row")
+            StyleableTutorialText(text = "1-) **Row** is a layout composable that places its children in a horizontal sequence.")
+            RowExample()
 
-    Column(
-        modifier = Modifier
-            .background(color = Color.LightGray)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Hello $name! $clicked")
-        Button(onClick = {
-            clicked = true
-        }) {
-            Text(text = "Click Me")
+            TutorialHeader(text = "Column")
+            StyleableTutorialText(text = "2-) **Column** is a layout composable that places its children in a vertical sequence.")
+            ColumnExample()
         }
     }
+}
+
+@Composable
+fun RowExample() {
+    TutorialText2(text = "Arrangement.Start")
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+        RowTexts()
+    }
+
+    TutorialText2(text = "Arrangement.End")
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+        RowTexts()
+    }
+
+    TutorialText2(text = "Arrangement.Center")
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        RowTexts()
+    }
+
+    TutorialText2(text = "Arrangement.SpaceEvenly")
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        RowTexts()
+    }
+
+    TutorialText2(text = "Arrangement.SpaceAround")
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+        RowTexts()
+    }
+
+    TutorialText2(text = "Arrangement.SpaceBetween")
+
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        RowTexts()
+    }
+}
+
+@Composable
+fun ColumnExample() {
+    val modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()
+        .height(200.dp)
+        .background(Color.LightGray)
+
+    TutorialText2(text = "Arrangement.Top")
+    Column(modifier = modifier, verticalArrangement = Arrangement.Top) {
+        ColumnTexts()
+    }
+
+    TutorialText2(text = "Arrangement.Bottom")
+    Column(modifier = modifier, verticalArrangement = Arrangement.Bottom) {
+        ColumnTexts()
+    }
+
+    TutorialText2(text = "Arrangement.Center")
+    Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
+        ColumnTexts()
+    }
+
+    TutorialText2(text = "Arrangement.SpaceEvenly")
+    Column(modifier = modifier, verticalArrangement = Arrangement.SpaceEvenly) {
+        ColumnTexts()
+    }
+
+    TutorialText2(text = "Arrangement.SpaceAround")
+    Column(modifier = modifier, verticalArrangement = Arrangement.SpaceAround) {
+        ColumnTexts()
+    }
+
+    TutorialText2(text = "Arrangement.SpaceBetween")
+    Column(modifier = modifier, verticalArrangement = Arrangement.SpaceBetween) {
+        ColumnTexts()
+    }
+}
+
+@Composable
+fun TutorialText2(text: String, modifier: Modifier = Modifier) {
+
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.body2,
+            color = Color.Gray,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp, top = 8.dp, bottom = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun RowTexts() {
+    Text(
+        text = "Row1", modifier = Modifier
+            .background(Color(0xFFFF9800))
+            .padding(4.dp)
+    )
+    Text(
+        text = "Row2", modifier = Modifier
+            .background(Color(0xFFFFA726))
+            .padding(4.dp)
+    )
+    Text(
+        text = "Row3", modifier = Modifier
+            .background(Color(0xFFFFB74D))
+            .padding(4.dp)
+    )
+}
+
+@Composable
+fun ColumnTexts() {
+    Text(
+        text = "Column1", modifier = Modifier
+            .background(Color(0xFF8BC34A))
+            .padding(4.dp)
+    )
+    Text(
+        text = "Column2", modifier = Modifier
+            .background(Color(0xFF9CCC65))
+            .padding(4.dp)
+    )
+    Text(
+        text = "Column3", modifier = Modifier
+            .background(Color(0xFFAED581))
+            .padding(4.dp)
+    )
+}
+
+@Composable
+fun TutorialHeader(text: String, modifier: Modifier = Modifier) {
+
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 12.dp, bottom = 6.dp),
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        text = text
+    )
+}
+
+val boldRegex = Regex("(?<!\\*)\\*\\*(?!\\*).*?(?<!\\*)\\*\\*(?!\\*)")
+
+@Composable
+fun StyleableTutorialText(text: String, modifier: Modifier = Modifier, bullets:Boolean = true) {
+
+    var results: MatchResult? = boldRegex.find(text)
+    val boldIndexes = mutableListOf<Pair<Int, Int>>()
+    val keywords = mutableListOf<String>()
+
+    var finalText = text
+
+    while (results != null) {
+        keywords.add(results.value)
+        results = results.next()
+    }
+
+    keywords.forEach { keyword ->
+        val newKeyWord = keyword.removeSurrounding("**")
+        finalText = finalText.replace(keyword, newKeyWord)
+        val indexOf = finalText.indexOf(newKeyWord)
+        boldIndexes.add(Pair(indexOf, indexOf + newKeyWord.length))
+    }
+
+    val annotatedString = buildAnnotatedString {
+
+        append(finalText)
+
+        if (bullets) {
+            addStyle(style = SpanStyle(fontWeight = FontWeight.Bold), start = 0, end = 3)
+        }
+
+        // Add bold style to keywords that has to be bold
+        boldIndexes.forEach {
+            addStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xff64B5F6),
+                    fontSize = 15.sp
+
+                ),
+                start = it.first,
+                end = it.second
+            )
+
+        }
+    }
+
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 12.dp, bottom = 12.dp),
+        fontSize = 16.sp,
+        text = annotatedString
+    )
 }
