cb6114eb03e5f6b3eb395179a89bd05476776854
adicomdotir <adicom.ir@gmail.com>
2022-12-06 19:50:10 +0330

    step-1

--- app/src/main/java/ir/adicom/myapplication/MainActivity.kt
+++ app/src/main/java/ir/adicom/myapplication/MainActivity.kt
@@ -1,25 +1,29 @@
 package ir.adicom.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import ir.adicom.myapplication.ui.theme.Compose_uiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Compose_uiTheme {
                 // A surface container using the 'background' color from the theme
@@ -29,16 +33,7 @@ class MainActivity : ComponentActivity() {
 //                ) {
 //                    Greeting("Android")
 //                }
-                Column(
-                    modifier = Modifier.background(color = Color.LightGray)
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Greeting(name = "Android")
-                    Greeting(name = "Android")
-                    Greeting(name = "Android")
-                }
+                Greeting(name = "Android")
             }
         }
     }
@@ -46,5 +41,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val context = LocalContext.current
+    var clicked by remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+        modifier = Modifier
+            .background(color = Color.LightGray)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello $name! $clicked")
+        Button(onClick = {
+            clicked = true
+        }) {
+            Text(text = "Click Me")
+        }
+    }
 }
