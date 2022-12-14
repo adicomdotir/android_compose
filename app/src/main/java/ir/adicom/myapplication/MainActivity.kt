3a817a17e64f2dcf3b881aa56b07328e4db1a810
adicomdotir <adicom.ir@gmail.com>
2022-12-15 00:23:47 +0330

    Add custom shapes sample

--- app/src/main/java/ir/adicom/myapplication/MainActivity.kt
+++ app/src/main/java/ir/adicom/myapplication/MainActivity.kt
@@ -3,13 +3,32 @@ package ir.adicom.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ir.adicom.myapplication.ui.theme.Compose_uiTheme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,25 +40,126 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    MyApp() {
-                        startProfileActivity(it)
-                    }
+                    MyApp()
                 }
             }
         }
     }
-
-    private fun startProfileActivity(puppy: Puppy) {
-        ProfileActivity.newIntent(this, puppy)
-    }
 }
 
 @Composable
-fun MyApp(navigateToProfile: (Puppy) -> Unit) {
+fun MyApp() {
     Scaffold(
         content = {
-            BarkHomeContent(navigateToProfile)
+            TicketComposable(modifier = Modifier.fillMaxSize())
         }
     )
+}
+
+@Composable
+fun TicketComposable(modifier: Modifier) {
+    Text(
+        text = "sample text".uppercase(),
+        style = TextStyle(
+            color = Color.White,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Black,
+        ),
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .wrapContentSize()
+            .graphicsLayer {
+                shadowElevation = 8.dp.toPx()
+                shape = TicketShape(24.dp.toPx())
+                clip = true
+            }
+            .background(color = Color.DarkGray)
+            .drawBehind {
+                scale(scale = 0.9f) {
+                    drawPath(
+                        path = drawTicketPath(size = size, cornerRadius = 24.dp.toPx()),
+                        color = Color.Red,
+                        style = Stroke(
+                            width = 2.dp.toPx(),
+                            pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f))
+                        )
+                    )
+                }
+            }
+            .padding(start = 32.dp, top = 64.dp, end = 32.dp, bottom = 64.dp)
+    )
+}
+
+class TicketShape(private val cornerRadius: Float) : Shape {
+
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        return Outline.Generic(
+            // Draw your custom path here
+            path = drawTicketPath(size = size, cornerRadius = cornerRadius)
+        )
+    }
+}
+
+fun drawTicketPath(size: Size, cornerRadius: Float): Path {
+    return Path().apply {
+        reset()
+        // Top left arc
+        arcTo(
+            rect = Rect(
+                left = -cornerRadius,
+                top = -cornerRadius,
+                right = cornerRadius,
+                bottom = cornerRadius
+            ),
+            startAngleDegrees = 90.0f,
+            sweepAngleDegrees = -90.0f,
+            forceMoveTo = false
+        )
+        lineTo(x = size.width - cornerRadius, y = 0f)
+        // Top right arc
+        arcTo(
+            rect = Rect(
+                left = size.width - cornerRadius,
+                top = -cornerRadius,
+                right = size.width + cornerRadius,
+                bottom = cornerRadius
+            ),
+            startAngleDegrees = 180.0f,
+            sweepAngleDegrees = -90.0f,
+            forceMoveTo = false
+        )
+        lineTo(x = size.width, y = size.height - cornerRadius)
+        // Bottom right arc
+        arcTo(
+            rect = Rect(
+                left = size.width - cornerRadius,
+                top = size.height - cornerRadius,
+                right = size.width + cornerRadius,
+                bottom = size.height + cornerRadius
+            ),
+            startAngleDegrees = 270.0f,
+            sweepAngleDegrees = -90.0f,
+            forceMoveTo = false
+        )
+        lineTo(x = cornerRadius, y = size.height)
+        // Bottom left arc
+        arcTo(
+            rect = Rect(
+                left = -cornerRadius,
+                top = size.height - cornerRadius,
+                right = cornerRadius,
+                bottom = size.height + cornerRadius
+            ),
+            startAngleDegrees = 0.0f,
+            sweepAngleDegrees = -90.0f,
+            forceMoveTo = false
+        )
+        lineTo(x = 0f, y = cornerRadius)
+        close()
+    }
 }
 
