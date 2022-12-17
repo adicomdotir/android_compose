cf886f1a4c3612c7bb5579b12871f1b55b56acef
adicomdotir <adicom.ir@gmail.com>
2022-12-17 21:19:39 +0330

    add navigation step-1

--- app/src/main/java/ir/adicom/myapplication/MainActivity.kt
+++ app/src/main/java/ir/adicom/myapplication/MainActivity.kt
@@ -5,12 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,6 +18,8 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -50,116 +50,49 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
     Scaffold(
-        content = {
-            TicketComposable(modifier = Modifier.fillMaxSize())
+        topBar = { TopBar() },
+        backgroundColor = colorResource(id = R.color.purple_700)
+    ) { padding ->
+        Box(modifier = Modifier.padding(padding)) {
+
         }
+    }
+}
+
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = {
+            Text(text = "My App", fontSize = 18.sp)
+        },
+        backgroundColor = colorResource(id = R.color.purple_500),
+        contentColor = Color.White
     )
 }
 
 @Composable
-fun TicketComposable(modifier: Modifier) {
-    Text(
-        text = "sample text".uppercase(),
-        style = TextStyle(
-            color = Color.White,
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Black,
-        ),
-        textAlign = TextAlign.Center,
-        modifier = modifier
-            .wrapContentSize()
-            .graphicsLayer {
-                shadowElevation = 8.dp.toPx()
-                shape = TicketShape(24.dp.toPx())
-                clip = true
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") {
+            MainScreen(navController = navController)
+        }
+        composable(
+            "details/{countryName}",
+            arguments = listOf(navArgument("countryName") { type = NavType.StringType })
+        ) { backStackEntry ->
+            backStackEntry.arguments?.getString("countryName")?.let { countryName ->
+                DetailsScreen(countryName = countryName)
             }
-            .background(color = Color.DarkGray)
-            .drawBehind {
-                scale(scale = 0.9f) {
-                    drawPath(
-                        path = drawTicketPath(size = size, cornerRadius = 24.dp.toPx()),
-                        color = Color.Red,
-                        style = Stroke(
-                            width = 2.dp.toPx(),
-                            pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f))
-                        )
-                    )
-                }
-            }
-            .padding(start = 32.dp, top = 64.dp, end = 32.dp, bottom = 64.dp)
-    )
-}
-
-class TicketShape(private val cornerRadius: Float) : Shape {
-
-    override fun createOutline(
-        size: Size,
-        layoutDirection: LayoutDirection,
-        density: Density
-    ): Outline {
-        return Outline.Generic(
-            // Draw your custom path here
-            path = drawTicketPath(size = size, cornerRadius = cornerRadius)
-        )
+        }
     }
 }
 
-fun drawTicketPath(size: Size, cornerRadius: Float): Path {
-    return Path().apply {
-        reset()
-        // Top left arc
-        arcTo(
-            rect = Rect(
-                left = -cornerRadius,
-                top = -cornerRadius,
-                right = cornerRadius,
-                bottom = cornerRadius
-            ),
-            startAngleDegrees = 90.0f,
-            sweepAngleDegrees = -90.0f,
-            forceMoveTo = false
-        )
-        lineTo(x = size.width - cornerRadius, y = 0f)
-        // Top right arc
-        arcTo(
-            rect = Rect(
-                left = size.width - cornerRadius,
-                top = -cornerRadius,
-                right = size.width + cornerRadius,
-                bottom = cornerRadius
-            ),
-            startAngleDegrees = 180.0f,
-            sweepAngleDegrees = -90.0f,
-            forceMoveTo = false
-        )
-        lineTo(x = size.width, y = size.height - cornerRadius)
-        // Bottom right arc
-        arcTo(
-            rect = Rect(
-                left = size.width - cornerRadius,
-                top = size.height - cornerRadius,
-                right = size.width + cornerRadius,
-                bottom = size.height + cornerRadius
-            ),
-            startAngleDegrees = 270.0f,
-            sweepAngleDegrees = -90.0f,
-            forceMoveTo = false
-        )
-        lineTo(x = cornerRadius, y = size.height)
-        // Bottom left arc
-        arcTo(
-            rect = Rect(
-                left = -cornerRadius,
-                top = size.height - cornerRadius,
-                right = cornerRadius,
-                bottom = size.height + cornerRadius
-            ),
-            startAngleDegrees = 0.0f,
-            sweepAngleDegrees = -90.0f,
-            forceMoveTo = false
-        )
-        lineTo(x = 0f, y = cornerRadius)
-        close()
+@Composable
+fun MainScreen(navController: NavController) {
+    val textState = remember { mutableStateOf(TextFieldValue("")) }
+    Column {
+        SearchView(textState)
+        CountryList(navController = navController, state = textState)
     }
 }
-
